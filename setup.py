4df47163ecbb951f039fe14a455ec125171aeb8a
SameerRanjanMansingh <sameerranjan0367@gmail.com>
2024-08-07 19:57:13 +0530

    file structure setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A smart video recommendation platform that understands your commands or searches to suggest and provide recommendations based on your preferences, including multiple video searches.',
+    author='sameer',
+    license='MIT',
+)
