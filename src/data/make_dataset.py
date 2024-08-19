dde1626b7673ecdf797977fb6b3976b88e6e4ca4
SameerRanjanMansingh <sameerranjan0367@gmail.com>
2024-08-19 11:13:23 +0530

    make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,10 +5,7 @@ import logging
 from src.features.build_features import initial_process, text_cleaning, add_new_feature
 from logger_module.my_logger import setup_custom_logger
 
-
 logger = setup_custom_logger("my_app", log_level=logging.INFO, log_file="model.log")
-
-
 
 
 def read_csv(file_path: str) -> pd.DataFrame:
@@ -24,10 +21,13 @@ def read_csv(file_path: str) -> pd.DataFrame:
     try:
         df = pd.read_csv(file_path)
         return df
-    except FileNotFoundError as e:
-        print(f"FileNotFoundError: {e} - Please check if the file path is correct.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    except FileNotFoundError as file_error:
+        print(f"FileNotFoundError: {file_error} - Please check if the file path is correct.")
+        raise
+
+    except Exception as general_error:
+        print(f"An error occurred: {general_error}")
+        raise
 
 
 def main(df: pd.DataFrame) -> pd.DataFrame:
@@ -50,10 +50,12 @@ def main(df: pd.DataFrame) -> pd.DataFrame:
         df["tags"] = df["tags"].apply(lambda x: x.replace("|", " ").replace('"', ''))
 
         return df
-    except KeyError as e:
-        print(f"KeyError: {e} - Please check if the column names are correct.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    except KeyError as error:
+        print(f"KeyError: {error} - Please check if the column names are correct.")
+        raise
+    except Exception as error:
+        print(f"An error occurred: {error}")
+        raise
 
 
 def description_data_process(df: pd.DataFrame) -> pd.DataFrame:
@@ -70,11 +72,12 @@ def description_data_process(df: pd.DataFrame) -> pd.DataFrame:
         df = add_new_feature(df)
         df["row_summary"] = df["row_summary"].apply(lambda x: x.replace("|", " ").replace('"', '').replace(" | ", " "))
         return df
-    except KeyError as e:
-        print(f"KeyError: {e} - Please check if the column names are correct.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
+    except KeyError as error:
+        print(f"KeyError: {error} - Please check if the column names are correct.")
+        raise
+    except Exception as error:
+        print(f"An error occurred: {error}")
+        raise
 
 
 def save_data(df: pd.DataFrame, file_name: str):
@@ -86,19 +89,37 @@ def save_data(df: pd.DataFrame, file_name: str):
     file_name (str): Name of the output CSV file.
     """
     try:
-        output_path = "/data/processed/"
+        output_path = "data/processed/"
         pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
         df.to_csv(output_path + file_name, index=False)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    except Exception as error:
+        print(f"An error occurred: {error}")
+        raise
 
 
 if __name__ == '__main__':
     try:
-        raw_data_path = r".../data/raw/INvideos.csv"
-        data = read_csv(file_path=raw_data_path)
+        IN_raw_data_path = r"data/raw/INvideos.csv"
+        US_raw_data_path = r"data/raw/USvideos.csv"
+        GB_raw_data_path = r"data/raw/GBvideos.csv"
 
-        data = main(df=data)
+        IN_data = read_csv(file_path=IN_raw_data_path)
+        US_data = read_csv(file_path=US_raw_data_path)
+        GB_data = read_csv(file_path=GB_raw_data_path)
+
+        IN = main(df=IN_data)
+        US = main(df=US_data)
+        GB = main(df=GB_data)
+
+
+        '''
+        data = pd.concat([IN, US, GB], axis=0)
+
+        Use whole data for more robust performance
+        '''
+
+        data=IN.iloc[:10000,:]
+        
         save_data(df=data, file_name="processed_data.csv")
 
         description_data = description_data_process(df=data)
@@ -106,9 +127,6 @@ if __name__ == '__main__':
 
         logger.info("Data cleaning completed")
 
-    except FileNotFoundError as e:
-        print(f"FileNotFoundError: {e} - Please check if the file path is correct.")
     except Exception as e:
         print(f"An error occurred during execution: {e}")
-
-
+        raise
