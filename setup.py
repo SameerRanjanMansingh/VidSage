6ea608882e2b3e0c334fbfb1d8f52f16f95316e6
SAMEER <137798921+SameerRanjanMansingh@users.noreply.github.com>
2024-08-31 12:23:40 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
-    packages=find_packages(),
+    name='vidsage',
+    packages=find_packages(include=['src', 'logger_module']),
     version='0.1.0',
     description='A smart video recommendation platform that understands your commands or searches to suggest and provide recommendations based on your preferences, including multiple video searches.',
     author='sameer',
